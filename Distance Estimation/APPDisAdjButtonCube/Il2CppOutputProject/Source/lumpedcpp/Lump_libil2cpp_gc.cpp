d668a4e4377b777c8264a4a9729f844365d74635
zy0531 <zhaoyu0531@126.com>
2024-03-10 17:02:14 -0500

    anchor

--- Distance Estimation/APPDisAdjButtonCube/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_gc.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\Brooke\Unity\Distance Estimation\APPDisAdjButtonCube\Il2CppOutputProject\IL2CPP\libil2cpp\gc\BoehmGC.cpp"
-#include "D:\Brooke\Unity\Distance Estimation\APPDisAdjButtonCube\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GCHandle.cpp"
-#include "D:\Brooke\Unity\Distance Estimation\APPDisAdjButtonCube\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GarbageCollector.cpp"
-#include "D:\Brooke\Unity\Distance Estimation\APPDisAdjButtonCube\Il2CppOutputProject\IL2CPP\libil2cpp\gc\NullGC.cpp"
-#include "D:\Brooke\Unity\Distance Estimation\APPDisAdjButtonCube\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrier.cpp"
-#include "D:\Brooke\Unity\Distance Estimation\APPDisAdjButtonCube\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrierValidation.cpp"
